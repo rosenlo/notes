5aea10cc866471129cd5dd3760e9f52006c1bb69
Rosenlo <rosenluov@gmail.com>
2021-03-04 16:24:05 +0800

    add N-ary Tree Node

--- arithmetic/leetcode/structure/tree/node.go
+++ arithmetic/leetcode/structure/tree/node.go
@@ -1,5 +1,12 @@
 package tree
 
+// Node represented as N-ary Tree Node
+type Node struct {
+	Val      int
+	Children []*Node
+}
+
+// TreeNode represented as Binary Tree Node
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -99,4 +106,86 @@ func TreeNode2Ints(root *TreeNode) []int {
 		n--
 	}
 	return ints[:n]
+}
+
+/**
+
+N-ary Tree
+
+1. 输出序列以层级表示，每个子树组通过 NULL 值分割
+2. 存在子树为 NULL 值的情况，如果遇到 NULL 值，表明当前节点无子数组
+
+*/
+func Ints2NaryTreeNode(ints []int) *Node {
+	size := len(ints)
+	if size == 0 {
+		return nil
+	}
+
+	root := &Node{Val: ints[0]}
+
+	if size == 1 {
+		return root
+	}
+
+	queue := []*Node{root}
+
+	children := make([]*Node, 0, size)
+
+	var node *Node
+	for i := 2; i < size; i++ {
+		node = queue[0]
+
+		if ints[i] == NULL {
+			if len(children) > 0 {
+				node.Children = append(node.Children, children...)
+				queue = append(queue[1:], children...)
+				children = []*Node{}
+			} else {
+				queue = queue[1:]
+			}
+		} else {
+			children = append(children, &Node{Val: ints[i]})
+		}
+
+	}
+	if len(children) != 0 {
+		node.Children = children
+	}
+
+	return root
+}
+
+func NaryTreeNode2Ints(node *Node) []int {
+	if node == nil {
+		return []int{}
+	}
+	ret := []int{node.Val}
+	queue := []*Node{node}
+
+	if len(node.Children) > 0 {
+		ret = append(ret, NULL)
+	}
+
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+
+		for _, children := range node.Children {
+			ret = append(ret, children.Val)
+		}
+		if len(node.Children) != 0 {
+			queue = append(queue, node.Children...)
+		}
+		if len(queue) > 0 {
+			ret = append(ret, NULL)
+		}
+	}
+	n := len(ret)
+
+	for n > 0 && ret[n-1] == NULL {
+		n--
+	}
+
+	return ret[:n]
 }
--- arithmetic/leetcode/structure/tree/node_test.go
+++ arithmetic/leetcode/structure/tree/node_test.go
@@ -6,6 +6,18 @@ import (
 	"testing"
 )
 
+func TestTreeOrder(t *testing.T) {
+	tests := [][]int{
+		{1, 2, 3, 4, 5, 6},
+	}
+	for i := 0; i < len(tests); i++ {
+		node := Ints2TreeNode(tests[i])
+		fmt.Printf("PreOrder:  %v\n", PreOrder(node))
+		fmt.Printf("InOrder:   %v\n", InOrder(node))
+		fmt.Printf("PostOrder: %v\n", PostOrder(node))
+	}
+}
+
 func TestInts2TreeNode(t *testing.T) {
 	tests := [][]int{
 		{1, 2, 3},
@@ -23,21 +35,28 @@ func TestInts2TreeNode(t *testing.T) {
 	for i := 0; i < len(tests); i++ {
 		node := Ints2TreeNode(tests[i])
 		ret := TreeNode2Ints(node)
-		t.Log(tests[i], ret)
+		fmt.Printf("[input]: %v\t", tests[i])
+		fmt.Printf("[expect]: %v\t", tests[i])
+		fmt.Printf("[output]: %v\t\n", ret)
 		if !reflect.DeepEqual(tests[i], ret) {
 			t.Fatalf("Wrong Answer, ret: %v right ret: %v", ret, tests[i])
 		}
 	}
 }
 
-func TestTreeOrder(t *testing.T) {
+func TestInts2NaryTreeNode(t *testing.T) {
 	tests := [][]int{
-		{1, 2, 3, 4, 5, 6},
+		{1, NULL, 3, 2, 4, NULL, 5, 6},
+		{1, NULL, 2, 3, 4, 5, NULL, NULL, 6, 7, NULL, 8, NULL, 9, 10, NULL, NULL, 11, NULL, 12, NULL, 13, NULL, NULL, 14},
 	}
 	for i := 0; i < len(tests); i++ {
-		node := Ints2TreeNode(tests[i])
-		fmt.Printf("PreOrder:  %v\n", PreOrder(node))
-		fmt.Printf("InOrder:   %v\n", InOrder(node))
-		fmt.Printf("PostOrder: %v\n", PostOrder(node))
+		node := Ints2NaryTreeNode(tests[i])
+		ret := NaryTreeNode2Ints(node)
+		fmt.Printf("[input]: %v\t", tests[i])
+		fmt.Printf("[expect]: %v\t", tests[i])
+		fmt.Printf("[output]: %v\t\n", ret)
+		if !reflect.DeepEqual(tests[i], ret) {
+			t.Fatalf("Wrong Answer, actual: %v expected: %v", ret, tests[i])
+		}
 	}
 }
